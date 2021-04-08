197c3bd174bde61b47980a7c4455c4d79b3746df
spouzols <spouzols@betomorrow.com>
2021-04-08 19:59:42 +0200

    Fix terraform-wrapper formula style

--- Formula/terraform-wrapper.rb
+++ Formula/terraform-wrapper.rb
@@ -1,15 +1,19 @@
 class TerraformWrapper < Formula
-	desc "Tiny Terraform wrapper for version and workspace variables"
-	homepage "https://github.com/BeTomorrow/terraform-wrapper"
-	head "https://github.com/BeTomorrow/terraform-wrapper"
-	url "https://github.com/BeTomorrow/terraform-wrapper/archive/v0.1.0-alpha.tar.gz"
-	sha256 "35d4c14e4f76ea4c43652da799cfded41a82ac28e392d4a66ad83a04d8e5079e"
+  desc "Tiny Terraform wrapper for version and workspace variables"
+  homepage "https://github.com/BeTomorrow/terraform-wrapper"
+  url "https://github.com/BeTomorrow/terraform-wrapper/archive/v0.1.0-alpha.tar.gz"
+  sha256 "35d4c14e4f76ea4c43652da799cfded41a82ac28e392d4a66ad83a04d8e5079e"
+  head "https://github.com/BeTomorrow/terraform-wrapper"
 
-	bottle :unneeded
+  bottle :unneeded
 
-	depends_on "tfenv"
+  depends_on "tfenv"
 
-	def install
-		bin.install "tfw"
-	end
+  def install
+    bin.install "tfw"
+  end
+
+  test do
+    assert_predicate bin/"tfw", :executable?
+  end
 end
