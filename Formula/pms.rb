5110f46e1741410f1ae87dfcf97629cbeb50c33b
spouzols <spouzols@betomorrow.com>
2023-06-16 10:52:40 +0200

    pms update v0.5.0-alpha

--- Formula/pms.rb
+++ Formula/pms.rb
@@ -1,8 +1,8 @@
 class Pms < Formula
   desc "Postman sync tool"
   homepage "https://github.com/BeTomorrow/pms"
-  url "https://github.com/BeTomorrow/pms/archive/v0.4.0-alpha.tar.gz"
-  sha256 "06ab19ff89f40d4b1aea58f4542880cedde13e60f6b34dd9bd630dfe2a4f415e"
+  url "https://github.com/BeTomorrow/pms/archive/v0.5.0-alpha.tar.gz"
+  sha256 "94faa4b42a7a6f1ff089a97d0e9e7edbb746804193a16ddd1d438fba731ae30f"
   head "https://github.com/BeTomorrow/pms"
 
   depends_on "jq"
